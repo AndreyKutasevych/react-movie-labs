b2c2ffb57a2e78c1f19dc1118d655cca57c8ac57
Andrii <andrdroniuc@gmail.com>
2025-10-17 22:25:31 +0100

    Added Upcoming movies page.

--- movies/src/main.jsx
+++ movies/src/main.jsx
@@ -8,8 +8,8 @@ import SiteHeader from './components/siteHeader';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import MoviesContextProvider from "./contexts/moviesContext";
-import AddMovieReviewPage from './pages/addMovieReviewPage'
-
+import AddMovieReviewPage from './pages/addMovieReviewPage';
+import UpcomingMoviesPage from './pages/upcomingMoviesPage';
 
 
 const queryClient = new QueryClient({
@@ -31,12 +31,13 @@ const App = () => {
         <SiteHeader />
         <MoviesContextProvider>
           <Routes>
+            <Route path="/reviews/form" element={<AddMovieReviewPage />} />
+            <Route path="/reviews/:id" element={<MovieReviewPage />} />
             <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
-            <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
+            <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} />
             <Route path="/movies/:id" element={<MoviePage />} />
             <Route path="/" element={<HomePage />} />
-            <Route path="*" element={ <Navigate to="/" /> } />
-            <Route path="/reviews/form" element={ <AddMovieReviewPage /> } />
+            <Route path="*" element={<Navigate to="/" />} />
           </Routes>
         </MoviesContextProvider>
       </BrowserRouter>
